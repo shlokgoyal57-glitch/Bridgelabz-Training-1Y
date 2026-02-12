1b636e04f0977b4137918cd072414e93c90fc23b
shlok goyal <shlokgoyal57@gmail.com>
2026-02-12 23:02:33 +0530

    Added String.java

---
+++ String.java/WithoutReverseString.java
@@ -0,0 +1,13 @@
+public class WithoutReverseString {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println(" Without Reversed String: " + reversed);
+    }
+}
